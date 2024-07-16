7466e6c1efd3b7f2a4e670d76c6cfcf9010c523c
Soojin <chsj5110@gmail.com>
2024-07-16 15:46:07 +0900

    24.07.16

--- restful-web-services/src/main/java/com/soojinChoi/rest/webservices/restful_web_services/user/UserDAOService.java
+++ restful-web-services/src/main/java/com/soojinChoi/rest/webservices/restful_web_services/user/UserDAOService.java
@@ -3,6 +3,7 @@ package com.soojinChoi.rest.webservices.restful_web_services.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -12,14 +13,33 @@ public class UserDAOService {
 	
 	private static List<User> users = new ArrayList<>();
 	
+	private static int usersCount = 0;
+	
 	static {
-		users.add(new User(1, "Soojin", LocalDate.now().minusYears(30)));
-		users.add(new User(1, "Hongrim", LocalDate.now().minusYears(28)));
-		users.add(new User(1, "Jason", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount, "Soojin", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Hongrim", LocalDate.now().minusYears(28)));
+		users.add(new User(++usersCount, "Jason", LocalDate.now().minusYears(20)));
 	}
 	
 	public List<User> findAll(){
 		return users;
 	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+	
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+	
+	public void deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		users.removeIf(predicate);
+	}
+	
 
 }
